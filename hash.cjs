535f700a56cffb320d95618df3c90764b6893747
Somajit Dey <dey.somajit@gmail.com>
2024-09-22 22:28:30 +0530

    Added crypto.hash()

--- hash.cjs
+++ hash.cjs
@@ -1,4 +1,10 @@
+/*
+Sources:
+https://nodejs.org/api/crypto.html#cryptohashalgorithm-data-outputencoding
+*/
+
 // Require the framework and instantiate it
 
-const {createHash} = require('node:crypto');
-console.log(createHash('md5').update('hello').digest('hex'));
+const {createHash, hash} = require('node:crypto');
+console.log(createHash('md5').update('hello').digest('hex')); // Legacy method
+console.log(hash('md5', 'hello', 'base64url')) // Faster method
